94843a556df704777ddf56fc54fda5c85101600a
Akshay peherkar <95123298+Akshayp02@users.noreply.github.com>
2021-12-10 07:10:17 +0530

    Commit

---
+++ a_to_z.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		
+	//	print a to z in revirce alfabets using asski value
+	
+		char i = 122;
+		System.out.println(" the value alfabet is :");
+		while(i >=97)
+		{
+			System.out.println( i);
+			i--;
+		}
+	}
+}
---
+++ a_to_z_For_loop.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		char i;
+		System.out.println("the alfabets is : /n");
+		for(i=97;	i <=122 ; i++)
+		 	{
+		 		System.out.println(i);
+		 	}
+	}
+}
